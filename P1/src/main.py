86b20434ffdcf4aa4ee4ea806dbaa2dbccefb826
ghazalb76 <ghazal.b93@gmail.com>
2019-04-08 23:13:36 +0430

    Read dictionary

---
+++ P1/src/main.py
@@ -0,0 +1,7 @@
+import csv
+with open("Entries.csv", encoding="utf8") as f:
+    reader = csv.reader(f)
+    next(reader) # skip header
+    data = []
+    for row in reader:
+        data.append(row)
