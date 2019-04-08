c891f453b9a7351875eaa33d2ab3b0ad1c14f6e5
ghazalb76 <ghazal.b93@gmail.com>
2019-04-08 23:16:21 +0430

    Read first input file

--- P1/src/main.py
+++ P1/src/main.py
@@ -5,3 +5,8 @@ with open("Entries.csv", encoding="utf8") as f:
     data = []
     for row in reader:
         data.append(row)
+f=open("in_1.txt", encoding="utf8")
+if f.mode == 'r':
+    contents =f.read()
+contents = contents.split()
+print(contents)
