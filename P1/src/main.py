ed0b1f0ef1a94df32cd010b1342dad1619393ced
ghazalb76 <ghazal.b93@gmail.com>
2019-04-08 23:36:25 +0430

    Find the words in dictionary

--- P1/src/main.py
+++ P1/src/main.py
@@ -1,5 +1,13 @@
 import csv
 
+# Read input
+f=open("in_1.txt", encoding="utf8")
+if f.mode == 'r':
+    contents =f.read()
+contents = contents.split()
+print(contents)
+
+# text_file = open("Output.txt", "w", encoding="utf8")
 
 # Read dictionary
 with open("Entries.csv", encoding="utf8") as f:
@@ -8,10 +16,11 @@ with open("Entries.csv", encoding="utf8") as f:
     data = []
     for row in reader:
         data.append(row)
+    for word in contents:
+        for dWord in data:
+            # print(word, "------>", dWord[1])
+            if word == dWord[1]:
+                print("YES")
+                # text_file.write(dWord[0])
 
-# Read input
-f=open("in_1.txt", encoding="utf8")
-if f.mode == 'r':
-    contents =f.read()
-contents = contents.split()
-print(contents)
+
