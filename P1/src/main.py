37b08a3cb58ef1ffe8afaf144dc85daee4a80db4
ghazalb76 <ghazal.b93@gmail.com>
2019-04-09 23:30:00 +0430

    Make dictionary for words

--- P1/src/main.py
+++ P1/src/main.py
@@ -8,7 +8,6 @@ f=open("in_1.txt", encoding="utf8")
 if f.mode == 'r':
     contents =f.read()
 contents = contents.split()
-print(contents)
 
 # text_file = open("Output.txt", "w", encoding="utf8")
 
@@ -22,9 +21,7 @@ with open("Entries.csv", encoding="utf8") as f:
     phonetics = []
     for word in contents:
         for dWord in data:
-            # print(word, "------>", dWord[1])
             if word == dWord[1]:
-                print("YES")
                 phonetics.append(dWord[0])
                 break
                 # text_file.write(dWord[0])
@@ -33,3 +30,14 @@ for t in phonetics:
     jomle+=t
 print(jomle)
 findSubString("football", " oot")
+liste = []
+for i in range(0, len(jomle)):
+    sub = jomle[0:i]
+    if any(sub in subl for subl in data):
+        liste.append(sub)
+liste.remove('')
+print(liste)
+dic = {}
+for r in liste:
+    dic[r] = []
+print(dic)
