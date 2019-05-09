4b2f59f3672f5c33f7f50a9f3b0b25076e2c9fe2
ghazalb76 <ghazal.b93@gmail.com>
2019-05-09 15:43:32 +0430

    Make wordCloud for pop

--- P2/WordCloud/lyrics_wordCloud.py
+++ P2/WordCloud/lyrics_wordCloud.py
@@ -1,4 +1,7 @@
 
+from wordcloud import WordCloud, STOPWORDS
+import matplotlib.pyplot as plt
+
 def pop_freq():
     pop_text = open("../ProcessedData/out_pop_lyrics.txt", 'r', encoding="utf-8")
 
@@ -23,11 +26,12 @@ def pop_freq():
 
     for word in pop_words_freq:
         pop_words_freq[word] /= (len(pop_words)/10000)
-
+    return pop_words
     # for word in pop_words_freq:
     #     print(pop_words_freq[word], ' : ', word)
 
 
+traditional_words = []
 def traditional_freq():
     traditional_text = open("../ProcessedData/out_traditional_lyrics.txt", 'r', encoding="utf-8")
 
@@ -36,7 +40,6 @@ def traditional_freq():
         print("Yes")
         traditional_word.append(line)
 
-    traditional_words = []
     for word in traditional_word:
         traditional_words = word.split(' ')
 
@@ -56,5 +59,27 @@ def traditional_freq():
     # for word in traditional_words_freq:
     #     print(traditional_words_freq[word], ' : ', word)
 
-pop_freq()
+
+def pop_cloud(pop_words):
+    pop_cloud_text = ''
+    for word in pop_words:
+        pop_cloud_text += ' '
+        pop_cloud_text += word
+    print(pop_cloud_text)
+    generate_cloud(pop_cloud_text)
+    
+
+
+def generate_cloud(my_wordCloud):
+    print('umad too')
+    popWordCloud = WordCloud(font_path='Far_KoodkBd.ttf', background_color='white', height=4000, width=4000)
+    popWordCloud.generate(my_wordCloud)
+    plt.imshow(popWordCloud)
+    plt.axis("off")
+
+    plt.show()
+
+pop_words = pop_freq()
 traditional_freq()
+
+pop_cloud(pop_words)
