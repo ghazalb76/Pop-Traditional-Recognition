857e54a78e94025e03d97616554abe9960f13c06
ghazalb76 <ghazal.b93@gmail.com>
2019-05-09 15:24:32 +0430

    Calculate frequency of traditional_words

--- P2/WordCloud/lyrics_wordCloud.py
+++ P2/WordCloud/lyrics_wordCloud.py
@@ -28,4 +28,33 @@ def pop_freq():
     #     print(pop_words_freq[word], ' : ', word)
 
 
+def traditional_freq():
+    traditional_text = open("../ProcessedData/out_traditional_lyrics.txt", 'r', encoding="utf-8")
+
+    traditional_word = []
+    for line in traditional_text.readlines():
+        print("Yes")
+        traditional_word.append(line)
+
+    traditional_words = []
+    for word in traditional_word:
+        traditional_words = word.split(' ')
+
+    traditional_words_freq = {}
+
+    for word in traditional_words:
+        if word in traditional_words_freq:
+            traditional_words_freq[word] += 1
+        else:
+            traditional_words_freq[word] = 1
+
+
+
+    for word in traditional_words_freq:
+        traditional_words_freq[word] /= (len(traditional_words)/10000)
+
+    # for word in traditional_words_freq:
+    #     print(traditional_words_freq[word], ' : ', word)
+
 pop_freq()
+traditional_freq()
