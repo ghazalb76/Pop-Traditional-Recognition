bde74488e6464ade710825f1174f4d83ea117eea
ghazalb76 <ghazal.b93@gmail.com>
2019-05-10 17:26:30 +0430

    Fix directories and code style

--- P2/WordCloud/src/lyrics_wordCloud.py
+++ P2/WordCloud/src/lyrics_wordCloud.py
@@ -1,15 +1,16 @@
+# -*- coding: utf-8 -*-
 
 from wordcloud import WordCloud, STOPWORDS
 import matplotlib.pyplot as plt
 import arabic_reshaper
 from bidi.algorithm import get_display
 
+
 def pop_freq():
-    pop_text = open("../ProcessedData/out_pop_lyrics.txt", 'r', encoding="utf-8")
+    pop_text = open("../../ProcessedData/pop/pop.txt", 'r', encoding="utf-8")
 
     pop_word = []
     for line in pop_text.readlines():
-        print("Yes")
         pop_word.append(line)
 
     pop_words = []
@@ -17,57 +18,47 @@ def pop_freq():
         pop_words = word.split(' ')
 
     pop_words_freq = {}
-
     for word in pop_words:
         if word in pop_words_freq:
             pop_words_freq[word] += 1
         else:
             pop_words_freq[word] = 1
 
-
-
     for word in pop_words_freq:
         pop_words_freq[word] /= (len(pop_words)/10000)
+
     return pop_words
-    # for word in pop_words_freq:
-    #     print(pop_words_freq[word], ' : ', word)
 
 
-traditional_words = []
 def traditional_freq():
-    traditional_text = open("../ProcessedData/out_traditional_lyrics.txt", 'r', encoding="utf-8")
+    traditional_text = open("../../ProcessedData/traditional/traditional.txt", 'r', encoding="utf-8")
 
     traditional_word = []
     for line in traditional_text.readlines():
-        print("Yes")
         traditional_word.append(line)
 
+    traditional_words = []
     for word in traditional_word:
         traditional_words = word.split(' ')
 
     traditional_words_freq = {}
-
     for word in traditional_words:
         if word in traditional_words_freq:
             traditional_words_freq[word] += 1
         else:
             traditional_words_freq[word] = 1
 
-
-
     for word in traditional_words_freq:
         traditional_words_freq[word] /= (len(traditional_words)/10000)
 
     return traditional_words
-    # for word in traditional_words_freq:
-    #     print(traditional_words_freq[word], ' : ', word)
+
 
 def diff_words(word_list1, word_list2):
     for word in word_list1:
         if word in word_list2:
             word_list1.remove(word)
             word_list2.remove(word)
-    print(len(word_list1))
     return word_list1
 
 
@@ -76,26 +67,24 @@ def prepare_cloud_text(words):
     for word in words:
         cloud_text += ' '
         cloud_text += word
-    print(cloud_text)
     reshaped_text = reshape_text(cloud_text)
     generate_cloud(reshaped_text)
-    
+
 
 def reshape_text(text):
     reshaped_text = arabic_reshaper.reshape(text)
     reshaped_text = get_display(reshaped_text)
     return reshaped_text
 
+
 def generate_cloud(my_wordCloud):
-    print('umad too')
+    print("Wait for creating wordcloud...")
     popWordCloud = WordCloud(font_path='Far_KoodkBd.ttf', background_color='white', height=4000, width=4000)
     popWordCloud.generate(my_wordCloud)
     plt.imshow(popWordCloud)
     plt.axis("off")
-    print("in tamome")
 
     plt.show()
-    print("kollan az in biroon bia")
 
 
 pop_words = pop_freq()
@@ -121,14 +110,13 @@ traditional_diff_pop_words = diff_words(traditional_words_copy, pop_words_copy)
 
 #---------------------------- StopWords ----------------------#
 stopword_list = []
-unImportant = open("stopwords.txt", 'r', encoding="utf-8")
+unImportant = open("../stopwords.txt", 'r', encoding="utf-8")
 
 
 for line in unImportant.readlines():
         stopword_list.append(line)
 for sentence in stopword_list:
         stopword_list = sentence.split(' ')
-print(stopword_list)
 
 #----------- pop ----------#
 pop_words_freeStop = []
@@ -142,8 +130,6 @@ for word in traditional_words:
         if word not in stopword_list:
                 traditional_words_freeStop.append(word)
 #--------------------------------------------------------------#
-
-
 
 
 pop_words_freeStop_copy = pop_words_freeStop
