d7d7cc002f758077764ed734e4ff8aed9a7d6603
ghazalb76 <ghazal.b93@gmail.com>
2019-05-13 23:13:59 +0430

    Seperate pop and traditional processed files

--- P2/ProcessedData/src/processing.py
+++ P2/ProcessedData/src/processing.py
@@ -6,38 +6,42 @@ import glob
 normalizer = Normalizer()
 
 def pop_processing():
-        all_texts = open("../pop/pop.txt", 'w', encoding="utf-8")
-        pop_text = ''
-
+        file_name_counter = 1
         for pop_lyric_file in glob.glob("../../Data/pop/*.txt"):
+                pop_text = ''
+                all_texts = open("../pop/"+str(file_name_counter)+".txt", 'w', encoding="utf-8")
+
                 txt_file = open(pop_lyric_file, 'r', encoding="utf-8")
                 for line in txt_file.readlines():
+                        print(line)
                         pop_text += line
 
-        pop_text = normalizer.normalize(pop_text)
-        pop_text = word_tokenize(pop_text)
+                pop_text = normalizer.normalize(pop_text)
+                pop_text = word_tokenize(pop_text)
 
-        for word in pop_text:
-                word += ' '
-                all_texts.write(word)
+                for word in pop_text:
+                        word += ' '
+                        all_texts.write(word)
+                file_name_counter += 1
 
 
 
 def traditional_processing():
-        all_texts = open("../traditional/traditional.txt", 'w', encoding="utf-8")
-        traditional_text = ''
-
+        file_name_counter = 1
         for traditional_lyric_file in glob.glob("../../Data/traditional/*.txt"):
+                traditional_text = ''
+                all_texts = open("../traditional/"+str(file_name_counter)+".txt", 'w', encoding="utf-8")
                 txt_file = open(traditional_lyric_file, 'r', encoding="utf-8")
                 for line in txt_file.readlines():
                         traditional_text += line
 
-        traditional_text = normalizer.normalize(traditional_text)
-        traditional_text = word_tokenize(traditional_text)
+                traditional_text = normalizer.normalize(traditional_text)
+                traditional_text = word_tokenize(traditional_text)
 
-        for word in traditional_text:
-                word += ' '
-                all_texts.write(word)
+                for word in traditional_text:
+                        word += ' '
+                        all_texts.write(word)
+                file_name_counter += 1
 
 
 pop_processing()
