0e32b0984df03e110e4062286adf632a34eb1010
ghazalb76 <ghazal.b93@gmail.com>
2019-05-15 15:26:18 +0430

    Create class for ngram

--- P2/Model/src/LanguageModel.py
+++ P2/Model/src/LanguageModel.py
@@ -3,7 +3,23 @@ from nltk.collocations import *
 from nltk import ngrams
 import glob
 
+class Ngram_manager():
+
+    def ngram_words(self, text, n):
+        n_gram = ngrams(text.split(), n)
+        fdist = nltk.FreqDist(n_gram)
+        n_gram = []
+        for t in fdist:
+            if n == 1:
+                n_gram.append(t[0])
+            elif n == 2:
+                n_gram.append(t[0]+' '+t[1])
+            elif n == 3:
+                n_gram.append(t[0]+' '+t[1]+' '+t[2])
+        return n_gram
+
 class Pop_manager():
+    ngram_manager = Ngram_manager()
     def __init__(self):
         self.read_files()
 
@@ -14,22 +30,14 @@ class Pop_manager():
             txt_file = open(pop_lyric_file, 'r', encoding="utf-8")
             for line in txt_file.readlines():
                     self.pop_text += line
+    
+    def ngram_handler(self):
+        one_gram_list = self.ngram_manager.ngram_words(self.pop_text, 1)
+        two_gram_list = self.ngram_manager.ngram_words(self.pop_text, 2)
+        three_gram_list = self.ngram_manager.ngram_words(self.pop_text, 3)
 
 
-def ngram_words(text, n):
-    n_gram = ngrams(text.split(), n)
-    fdist = nltk.FreqDist(n_gram)
-    n_gram = []
-    for t in fdist:
-        if n == 1:
-            n_gram.append(t[0])
-        elif n == 2:
-            n_gram.append(t[0]+' '+t[1])
-        elif n == 3:
-            n_gram.append(t[0]+' '+t[1]+' '+t[2])
-    return n_gram
+
 
 pop_manager = Pop_manager()
-one_gram_list = ngram_words(pop_manager.pop_text, 1)
-two_gram_list = ngram_words(pop_manager.pop_text, 2)
-three_gram_list = ngram_words(pop_manager.pop_text, 3)
+pop_manager.ngram_handler()
