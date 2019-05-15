d1f9a4522b89d13f22fd27270740c4d296fdc57c
ghazalb76 <ghazal.b93@gmail.com>
2019-05-15 15:10:47 +0430

    Find one_gram words in pop_text

--- P2/Model/src/LanguageModel.py
+++ P2/Model/src/LanguageModel.py
@@ -14,15 +14,15 @@ class Pop_manager():
             txt_file = open(pop_lyric_file, 'r', encoding="utf-8")
             for line in txt_file.readlines():
                     self.pop_text += line
-        print(self.pop_text)
+
 
 def ngram_words(text, n):
-
-    n_gram = ngrams(sentence.split(), n)
+    n_gram = ngrams(text.split(), n)
     fdist = nltk.FreqDist(n_gram)
     n_gram = []
-    for t,v in fdist:
-        n_gram.append(t+' '+v)
+    for t in fdist:
+        n_gram.append(t[0])
     return n_gram
 
 pop_manager = Pop_manager()
+one_gram = ngram_words(pop_manager.pop_text, 1)
