2c28293640a94d7507e8d69b70b7fd63412a6006
ghazalb76 <ghazal.b93@gmail.com>
2019-05-15 12:26:02 +0430

    Fix pop and traditional seperation bug

--- P2/SplitData/src/split.py
+++ P2/SplitData/src/split.py
@@ -24,28 +24,38 @@ for path in dirpath_traditional :
 
 # Choose random files for test
 random_num = len(files_path)*0.2
-random_pathes = np.random.choice(files_path, int(random_num))
-test_pathes = np.split(random_pathes, 1)
-
+random_pathes = []
+while (len(random_pathes) < random_num):
+        random_path = np.random.choice(files_path, 1)
+        if random_path not in random_pathes:
+                random_pathes.append(random_path[0])
 
 # Seperate train and test
 test_pop_counter = 1
 test_traditional_counter = 1
-for path in test_pathes:
-        print(path)
-        for i in path:
-                if i in pop_files_path:
-                        pop_dest = "../test/pop/"+str(test_pop_counter)+".txt"
-                        copyfile(i, pop_dest)
-                        test_pop_counter += 1
-                        print(i)
-                else:
-                        traditional_dest = "../test/traditional/"+str(test_traditional_counter)+".txt"
-                        copyfile(i, traditional_dest)
-                        test_traditional_counter += 1
-                print(i)
-                files_path.remove(i)
-                        
+for i in random_pathes:
+        if i in pop_files_path:
+                pop_dest = "../test/pop/"+str(test_pop_counter)+".txt"
+                copyfile(i, pop_dest)
+                test_pop_counter += 1
+        else:
+                traditional_dest = "../test/traditional/"+str(test_traditional_counter)+".txt"
+                copyfile(i, traditional_dest)
+                test_traditional_counter += 1
+        files_path.remove(i)
 
-print(len(files_path), len(np.random.choice(files_path, int(random_num))))
+# Copy train files from Process part to split
+test_pop_counter = 1
+test_traditional_counter = 1
+# print(files_path)
+for train_path in files_path:
+        print(train_path)
+        if train_path in pop_files_path:
+                pop_dest = "../train/pop/"+str(test_pop_counter)+".txt"
+                copyfile(train_path, pop_dest)
+                test_pop_counter += 1
+        else:
+                traditional_dest = "../train/traditional/"+str(test_traditional_counter)+".txt"
+                copyfile(train_path, traditional_dest)
+                test_traditional_counter += 1
 
