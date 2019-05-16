7b48c085774cb32973ba501a289c0f5fb345cb40
ghazalb76 <ghazal.b93@gmail.com>
2019-05-16 11:52:33 +0430

    Complete unigram

--- P2/Model/src/LanguageModel.py
+++ P2/Model/src/LanguageModel.py
@@ -5,22 +5,21 @@ import glob
 from collections import Counter
 
 class Ngram_manager():
-
+    ngram_list = []
     def ngram_words(self, text, n):
         n_gram = ngrams(text.split(), n)
         fdist = nltk.FreqDist(n_gram)
-        print (Counter(n_gram))
         for t in fdist:
             if n == 1:
-                n_gram.append(t[0])
+                self.ngram_list.append(t[0])
             elif n == 2:
-                n_gram.append(t[0]+' '+t[1])
+                self.ngram_list.append(t[0]+' '+t[1])
             elif n == 3:
-                n_gram.append(t[0]+' '+t[1]+' '+t[2])
-        return n_gram
-    
-    # def calculate_onegram(unigram_list):
+                self.ngram_list.append(t[0]+' '+t[1]+' '+t[2])
+        return self.ngram_list
 
+    def calculate_probability(self, thisWord, wholeWord, types):
+        return (thisWord+1)/(wholeWord+types+1)
 
 
 class Pop_manager():
@@ -31,7 +30,7 @@ class Pop_manager():
     def __init__(self):
         self.read_files()
         self.make_dict()
-        print(self.pop_dict)
+        print(len(self.pop_dict))
 
     def read_files(self):
         for pop_lyric_file in glob.glob("../../SplitData/train/pop/*.txt"):
@@ -51,6 +50,14 @@ class Pop_manager():
     
     def ngram_handler(self):
         unigram_list = self.ngram_manager.ngram_words(self.pop_text, 1)
+        wholeWords = 0
+        unigram_counts = []
+        for word in self.pop_dict:
+            wholeWords += self.pop_dict[word]
+
+        for unigram in unigram_list:
+            unigram_counts.append((self.ngram_manager.calculate_probability(self.pop_dict[unigram], wholeWords, len(self.pop_dict)))*1000)
+        print(unigram_counts)
         # two_gram_list = self.ngram_manager.ngram_words(self.pop_text, 2)
         # three_gram_list = self.ngram_manager.ngram_words(self.pop_text, 3)
         # unigram = self.ngram_manager.calculate_onegram(unigram_list)
@@ -62,4 +69,4 @@ class Pop_manager():
 
 
 pop_manager = Pop_manager()
-# pop_manager.ngram_handler()
+pop_manager.ngram_handler()
