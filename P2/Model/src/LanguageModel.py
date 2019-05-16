723c7af7cf340076a8ddaa3b99deea1e5943632c
ghazalb76 <ghazal.b93@gmail.com>
2019-05-16 10:47:25 +0430

    Add list for pop words

--- P2/Model/src/LanguageModel.py
+++ P2/Model/src/LanguageModel.py
@@ -2,13 +2,14 @@ import nltk
 from nltk.collocations import *
 from nltk import ngrams
 import glob
+from collections import Counter
 
 class Ngram_manager():
 
     def ngram_words(self, text, n):
         n_gram = ngrams(text.split(), n)
         fdist = nltk.FreqDist(n_gram)
-        n_gram = []
+        print (Counter(n_gram))
         for t in fdist:
             if n == 1:
                 n_gram.append(t[0])
@@ -17,8 +18,14 @@ class Ngram_manager():
             elif n == 3:
                 n_gram.append(t[0]+' '+t[1]+' '+t[2])
         return n_gram
+    
+    # def calculate_onegram(unigram_list):
+
+
 
 class Pop_manager():
+    pop_v = {}
+    pop_words = []
     ngram_manager = Ngram_manager()
     def __init__(self):
         self.read_files()
@@ -30,14 +37,21 @@ class Pop_manager():
             txt_file = open(pop_lyric_file, 'r', encoding="utf-8")
             for line in txt_file.readlines():
                     self.pop_text += line
+
+        self.pop_words = self.pop_text.split(' ')
+        print(self.pop_words)
     
     def ngram_handler(self):
-        one_gram_list = self.ngram_manager.ngram_words(self.pop_text, 1)
-        two_gram_list = self.ngram_manager.ngram_words(self.pop_text, 2)
-        three_gram_list = self.ngram_manager.ngram_words(self.pop_text, 3)
+        unigram_list = self.ngram_manager.ngram_words(self.pop_text, 1)
+        # two_gram_list = self.ngram_manager.ngram_words(self.pop_text, 2)
+        # three_gram_list = self.ngram_manager.ngram_words(self.pop_text, 3)
+        # unigram = self.ngram_manager.calculate_onegram(unigram_list)
+
+    # def pop_types(self):
+
 
 
 
 
 pop_manager = Pop_manager()
-pop_manager.ngram_handler()
+# pop_manager.ngram_handler()
