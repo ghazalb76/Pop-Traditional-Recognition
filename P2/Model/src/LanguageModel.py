fa8d15f3e4d0d3878fdc8e45f91a07d1157a6cca
ghazalb76 <ghazal.b93@gmail.com>
2019-05-16 18:34:55 +0430

    Add tokens for first and last of sentences

--- P2/Model/src/LanguageModel.py
+++ P2/Model/src/LanguageModel.py
@@ -29,7 +29,7 @@ class Ngram_manager():
 
 
 class Pop_manager():
-    pop_text = ''
+    pop_text = '<s> '
     pop_dict = {}
     pop_words = []
     ngram_manager = Ngram_manager()
@@ -44,29 +44,36 @@ class Pop_manager():
             txt_file = open(pop_lyric_file, 'r', encoding="utf-8")
             for line in txt_file.readlines():
                 line = line.rstrip()
-                line += ' '
+                line += ' </s> <s> '
                 self.pop_text += line
+            # print(self.pop_text)
 
-        self.pop_words = self.pop_text.split(' ')
+        pop_wordss = self.pop_text.split(' ')
+        for i in range(0,len(pop_wordss)-2):
+            self.pop_words.append(pop_wordss[i])
         for word in self.pop_words:
             if word is '':
                 self.pop_words.remove('')
+        # print(self.pop_words)
 
     def make_dict(self):
         for word in self.pop_words:
-            if word in self.pop_dict:
-                self.pop_dict[word] += 1
-            else:
-                self.pop_dict[word] = 1
+                if word in self.pop_dict:
+                    self.pop_dict[word] += 1
+                else:
+                    self.pop_dict[word] = 1
     
     def ngram_handler(self):
         unigram_list = self.ngram_manager.ngram_words(self.pop_text, 1)
+        unigram_list.remove('<s>')
+        unigram_list.remove('</s>')
         wholeWords = 0
         unigram_counts = []
         for word in self.pop_dict:
-            wholeWords += self.pop_dict[word]
+            if word != '<s>' and word != '</s>':
+                wholeWords += self.pop_dict[word]
         for unigram in unigram_list:
-            unigram_counts.append((self.ngram_manager.calculate_probability(self.pop_dict[unigram], wholeWords, len(self.pop_dict))))
+            unigram_counts.append((self.ngram_manager.calculate_probability(self.pop_dict[unigram], wholeWords, len(self.pop_dict)-2)))
         print("************* unigram ************")
         for i in range(0,len(unigram_list)):
             print(unigram_list[i], "|", unigram_counts[i])
@@ -74,23 +81,21 @@ class Pop_manager():
  
         print("************* biagram ************")
         two_gram_list = self.ngram_manager.ngram_words(self.pop_text, 2)
+        two_gram_list_distinct = []
         for word in two_gram_list:
+            if word not in two_gram_list_distinct:
+                two_gram_list_distinct.append(word)
+
+        for word in two_gram_list_distinct:
             counter = 0
             words = word.split(' ')
-            for i in range(0, len(self.pop_words)-1):
-                if self.pop_words[i] == words[0] and self.pop_words[i+1] == words[1]:
-                    counter +=1
-            biagram_counts = self.ngram_manager.calculate_probability(counter, self.pop_dict[words[0]], len(self.pop_dict))
-            print(words[0], "|", words[1], "|", biagram_counts)
+            if words[0] != '</s>':
+                for i in range(0, len(self.pop_words)-1):
+                    if self.pop_words[i] == words[0] and self.pop_words[i+1] == words[1]:
+                        counter +=1
+                biagram_counts = self.ngram_manager.calculate_probability(counter, self.pop_dict[words[0]], len(self.pop_dict)-2)
+                print(words[0], "|", words[1], "|", biagram_counts)
         print("\n\n")
-
-
-
-        # three_gram_list = self.ngram_manager.ngram_words(self.pop_text, 3)
-
-    # def pop_types(self):
-
-
 
 
 
