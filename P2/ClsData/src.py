b97829e379eef02bbb51d9413328e9e0527b5877
ghazalb76 <ghazal.b93@gmail.com>
2019-06-04 00:00:44 +0430

    Seperate train and test successfully

--- P2/ClsData/src.py
+++ P2/ClsData/src.py
@@ -1,15 +1,51 @@
 import glob
+import random
 
 
-
-def label_pop(text):
+def label_pop(text_list):
 
     for pop_lyric_file in glob.glob("../ProcessedData/pop/*.txt"):
         txt_file = open(pop_lyric_file, 'r', encoding="utf-8")
-        print(pop_lyric_file)
         for line in txt_file.readlines():
-            text += "pop "+ line
-    out.write(text)
+            text_list.append("pop "+ line) 
+    return text_list
 
-out = open("out.txt", 'w', encoding="utf-8")
-label_pop('')
+
+def label_traditional(text_list):
+
+    for traditional_lyric_file in glob.glob("../ProcessedData/traditional/*.txt"):
+        txt_file = open(traditional_lyric_file, 'r', encoding="utf-8")
+        for line in txt_file.readlines():
+            text_list.append("traditional "+ line)
+    return text_list
+
+
+def seperate_train_test(text_list):
+
+    test_lines = []
+    train_text = ''
+    test_text = ''
+    i = 0
+    while i != int(0.2*len(text_list)):
+        random_line = random.randint(0, len(text_list)-1)
+        if text_list[random_line] not in test_lines:
+            test_lines.append(text_list[random_line])
+            i += 1
+    for line in text_list:
+        if line in test_lines:
+            test_text += line
+            test_lines.pop(test_lines.index(line))
+        else:
+            train_text += line
+    print(i)
+    return train_text, test_text
+
+
+text_list = label_pop([])
+text_list = label_traditional(text_list)
+train_text, test_text = seperate_train_test(text_list)
+
+train = open("train.txt", 'w', encoding="utf-8")
+test = open("test.txt", 'w', encoding="utf-8")
+train.write(train_text)
+test.write(test_text)
