94bcfc867064627cf7b3323e137a47356a0e4c57
ghazalb76 <ghazal.b93@gmail.com>
2019-06-04 20:27:17 +0430

    Add probability of classes

--- P2/ClsModel/NaiveBAyes/src.py
+++ P2/ClsModel/NaiveBAyes/src.py
@@ -11,14 +11,18 @@ def stopwords():
     return stopword_list
 
 
-def train():
-    train_file = open("../../ClsData/train.txt", 'r', encoding="utf-8")
+def train(c1, c2):
+    train_file = open("../../ClsData/TestCase/train.txt", 'r', encoding="utf-8")
     text = ""
     for line in train_file:
         line = line.rstrip()
         line = line.split(' ')
+        if line[0] == 'c1':
+            c1 += 1
+        elif line[0] == 'c2':
+            c2 += 1
         for word in line:
-            if line[0] == 'pop' and word != 'pop' and word not in stopword_list and word != "\n":
+            if line[0] == 'c1' and word != 'c1' and word not in stopword_list and word != "\n":
                 if word in pop_dict:
                     pop_dict[word] += 1
                 else:
@@ -28,7 +32,8 @@ def train():
                 else:
                     total_words_dict[word] = 1
 
-            elif line[0] == 'traditional' and word != 'traditional' and word not in stopword_list:
+            elif line[0] == 'c2' and word != 'c2' and word not in stopword_list:
+
                 if word in traditional_dict:
                     traditional_dict[word] += 1
                 else:
@@ -37,6 +42,7 @@ def train():
                     total_words_dict[word] += 1
                 else:
                     total_words_dict[word] = 1
+    return c1, c2
 
 
 def naive_algo():
@@ -61,14 +67,14 @@ def naive_algo():
 def test():
     out_text = ''
     measure_text = ''
-    test_file = open("../../ClsData/test.txt", 'r', encoding="utf-8")
+    test_file = open("../../ClsData/TestCase/test.txt", 'r', encoding="utf-8")
     for line in test_file:
-        pop_prob = 0
-        traditional_prob = 0
+        pop_prob = math.log(c1/(c1+c2), 10)
+        traditional_prob = math.log(c2/(c1+c2), 10)
         line = line.rstrip()
         line = line.split(' ')
         for word in line:
-            if word != 'pop' and word != 'traditional' and word not in stopword_list:
+            if word != 'c1' and word != 'c2' and word not in stopword_list:
                 if word in pop_dict:
                     pop_prob += pop_freq_dict[word]
                     # print(word, pop_freq_dict[word])
@@ -79,10 +85,10 @@ def test():
                     traditional_prob += traditional_freq_dict[word]
                 else:
                     traditional_prob += traditional_freq_dict['UNK']
-        out_text += ' pop '+ str(pop_prob) +' traditional '+ str(traditional_prob)
+        out_text += ' c1 '+ str(pop_prob) +' c2 '+ str(traditional_prob) + '\n'
 
         if traditional_prob > pop_prob:
-            if line[0] == 'traditional' :
+            if line[0] == 'c2' :
                 # print('traditional --> traditional')
                 traditional_measures['tp'] += 1
                 pop_measures['tn'] += 1
@@ -91,7 +97,7 @@ def test():
                 traditional_measures['fp'] += 1
                 pop_measures['fn'] += 1
         else :
-            if line[0] == 'traditional':
+            if line[0] == 'c2':
                 # print('traditional --> pop')
                 traditional_measures['fn'] += 1
                 pop_measures['fp'] += 1
@@ -106,14 +112,14 @@ def test():
     pop_precision = pop_measures['tp'] / (pop_measures['tp'] + pop_measures['fp'])
     pop_recall = pop_measures['tp'] / (pop_measures['tp'] + pop_measures['fn'])
     pop_F1 = (2 * pop_precision * pop_recall) / (pop_precision + pop_recall)
-    print ('Pop accuracy percentage : ', pop_accuracy*100)
-    print ('Pop Precision percentage : ', pop_precision*100)
-    print ('Pop Recall percentage : ', pop_recall*100)
-    print ('Pop F1 percentage : ', pop_F1*100)
-    measure_text += ('Pop accuracy percentage : ' + str(pop_accuracy*100) + '\n' +
-                    'Pop Precision percentage : ' + str(pop_precision*100) + '\n' +
-                    'Pop Recall percentage : ' + str(pop_recall*100) + '\n' +
-                    'Pop F1 percentage : ' + str(pop_F1*100) + '\n\n')
+    print ('c1 accuracy percentage : ', pop_accuracy*100)
+    print ('c1 Precision percentage : ', pop_precision*100)
+    print ('c1 Recall percentage : ', pop_recall*100)
+    print ('c1 F1 percentage : ', pop_F1*100)
+    measure_text += ('c1 accuracy percentage : ' + str(pop_accuracy*100) + '\n' +
+                    'c1 Precision percentage : ' + str(pop_precision*100) + '\n' +
+                    'c1 Recall percentage : ' + str(pop_recall*100) + '\n' +
+                    'c1 F1 percentage : ' + str(pop_F1*100) + '\n\n')
 
     print()
 
@@ -124,17 +130,18 @@ def test():
     traditional_precision = traditional_measures['tp'] / (traditional_measures['tp'] + traditional_measures['fp'])
     traditional_recall = traditional_measures['tp'] / (traditional_measures['tp'] + traditional_measures['fn'])
     traditional_F1 = (2 * traditional_precision * traditional_recall) / (traditional_precision + traditional_recall)
-    print ('Traditional accuracy percentage : ', traditional_accuracy*100)
-    print ('Traditional Precision : ', traditional_precision*100)
-    print ('Traditional Recall : ', traditional_recall*100)
-    print ('Traditional F1 : ', traditional_F1*100)
-    measure_text += ('Traditional accuracy percentage : ' + str(traditional_accuracy*100) + '\n' +
-                    'Traditional Precision : ' + str(traditional_precision*100) + '\n' +
-                    'Traditional Recall : ' + str(traditional_recall*100) + '\n' +
-                    'Traditional F1 : ' + str(traditional_F1*100))
+    print ('c2 accuracy percentage : ', traditional_accuracy*100)
+    print ('c2 Precision : ', traditional_precision*100)
+    print ('c2 Recall : ', traditional_recall*100)
+    print ('c2 F1 : ', traditional_F1*100)
+    measure_text += ('c2 accuracy percentage : ' + str(traditional_accuracy*100) + '\n' +
+                    'c2 Precision : ' + str(traditional_precision*100) + '\n' +
+                    'c2 Recall : ' + str(traditional_recall*100) + '\n' +
+                    'c2 F1 : ' + str(traditional_F1*100))
 
 
     return out_text, measure_text
+
 
 
 total_words_dict = {}
@@ -149,14 +156,14 @@ pop_measures = {'tp':0, 'tn':0, 'fp':0, 'fn':0}
 traditional_measures = {'tp':0, 'tn':0, 'fp':0, 'fn':0}
 
 stopword_list = stopwords()
-train()
+c1, c2 = train(0, 0)
 naive_algo()
 out_text, measure_text = test()
 
-test_out = open("Test.output.txt", 'w', encoding="utf-8")
+test_out = open("TestCase.output.txt", 'w', encoding="utf-8")
 test_out.write(out_text)
 
-test_out_report = open("Test.report.txt", 'w', encoding="utf-8")
+test_out_report = open("TestCase.report.txt", 'w', encoding="utf-8")
 test_out_report.write(measure_text)
 
 
